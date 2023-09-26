43437467ff6f6ccae98ae715929f2569ef0a0bab
Ashwin V <klprogrammer39@gmail.com>
2023-09-26 19:27:26 +0530

    Built Basic Navbar and dummy products array created, these products are not passed to index.hbs yet (Initial Commit from development Side)

---
+++ routes/index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  let products = [
+    {name: 'Boat Earbuds',
+    category : 'Audio Accessaries',
+    description : 'Best Budget Earbuds',
+    price : '1200 /-',
+    image: 'https://www.boat-lifestyle.com/cdn/shop/products/main_black_fa1c6ec3-93b7-443e-ae82-d5eeb34258f8_600x.png?v=1641206192'
+
+  
+  },
+  {
+    name: 'Boat Ring',
+    category : 'Electronic Accessories',
+    descripton : 'Futuristic Ring',
+    price: '8999 /-',
+    image: 'https://www.boat-lifestyle.com/cdn/shop/files/SmartRing-FI02_1500x.png?v=1692966075'
+  }
+  ]
+  res.render('index', { title: 'Express' });
+});
+
+
+
+module.exports = router;
