f44dc1bb00c15100aedde8486c31456d061c998a
Ashwin V <klprogrammer39@gmail.com>
2023-09-26 21:38:40 +0530

    admin view-products.hbs table added currently dummy products array used

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ var logger = require('morgan');
 var hbs=require('express-handlebars');
 
 
-var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var adminRouter = require('./routes/admin');
 
 
 var app = express();
@@ -23,8 +23,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', usersRouter);
+app.use('/admin', adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
