fe948e5f8c3efe2a7001d5c9dee215f95fbcde43
Ashwin V <klprogrammer39@gmail.com>
2023-09-27 16:52:32 +0530

    Admin Add Product Image storing in Public folder done

--- routes/admin.js
+++ routes/admin.js
@@ -31,9 +31,20 @@ router.get('/add-product', function(req,res){
 
 router.post('/add-product', (req,res)=>{
   console.log(req.body)
-  console.log(req.files.Image)
-  productHelper.addProduct(req.body, (result)=>{
-    res.render('admin/add-product')
+  console.log(req.files.productImage)
+  
+  productHelper.addProduct(req.body, (id)=>{
+    let image = req.files.productImage
+    image.mv('./public/product-images/'+id+'.jpg', (err,done)=>{
+      if(!err){
+            res.render('admin/add-product')
+
+
+      }else{
+        console.log('Error occured while Image storing '+ err)
+      }
+
+    })
   } )
 })
 
