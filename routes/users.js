d2af344c99b09324606befa2a8ceffc6a2464383
Ashwin V <klprogrammer39@gmail.com>
2023-10-02 21:09:23 +0530

    Cart totalValue display done

--- routes/users.js
+++ routes/users.js
@@ -72,8 +72,9 @@ router.get('/', async function(req, res, next) {
 
    router.get('/cart', verifyLogin,async (req,res)=>{
     let products = await userHelpers.getCartProducts(req.session.user._id)
+    let totalValue = await userHelpers.getTotalAmount(req.session.user._id)
     console.log(products)
-    res.render('user/cart', {products, user:req.session.user})
+    res.render('user/cart', {products, user:req.session.user,totalValue})
    })
 
    router.get('/add-to-cart/:id',async (req,res)=>{
